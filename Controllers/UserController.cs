c6a12a57b9cea67c970dc375339d14395921bc7a
hirenchandani <hirendra69@gmail.com>
2023-01-02 20:08:37 +0530

    Api Project Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+
+    }
+}
