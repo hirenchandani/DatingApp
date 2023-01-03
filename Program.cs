c9b7a3dd82b3a46c7dd1aaee6e70bf4437b21398
hirenchandani <hirendra69@gmail.com>
2023-01-03 16:02:41 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
+
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
@@ -27,6 +32,8 @@ var app = builder.Build();
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
+
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
